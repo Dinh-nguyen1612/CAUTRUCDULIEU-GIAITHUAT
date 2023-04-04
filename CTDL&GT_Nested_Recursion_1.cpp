aa2092ecb878cc5c25482560e959408f286241bd
Dinh-nguyen1612 <124942296+Dinh-nguyen1612@users.noreply.github.com>
2023-04-04 16:48:02 +0700

    Add files via upload

---
+++ CTDL&GT_Nested_Recursion_1.cpp
@@ -0,0 +1,12 @@
+//https://freetuts.net/de-quy-long-nested-recursion-2959.html
+#include <iostream>
+using namespace std;
+ 
+int A(int m, int n){
+  if(m == 0)
+    return n + 1;
+  else if(n == 0) 
+    return A(m - 1, 1);
+  else 
+    return A(m-1, A(m, n-1));
+}
